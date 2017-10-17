0e310a3c05ead12b085f82214a7f071247ffbc5c
ProjetoAlvesMira <projetoalvesmira@gmail.com>
2017-10-17 18:12:48 -0200

    QuimiCrush 1.0

---
+++ Assets/Scripts/GridItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridItem : MonoBehaviour {
+
+    public int x { get; private set; }
+    public int y { get; private set; }
+
+    public void OnItemPositionChange(int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+        gameObject.name = string.Format("Sprite [{0}] [{1}]", x, y);
+    }
+
+    private void OnMouseDown()
+    {
+        if (OnMouseOverItemEventHandler != null)
+            OnMouseOverItemEventHandler(this);
+    }
+
+    public delegate void OnMOuseOverItem(GridItem item);
+    public static event OnMOuseOverItem OnMouseOverItemEventHandler;
+
+
+
+
+}
